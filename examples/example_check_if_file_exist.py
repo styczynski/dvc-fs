9dada80e47f955b28eb6f41aa5ca29cc54c98061
Hasan Ozdemir <hozdemir@covidgenomics.com>
2022-06-06 18:14:23 +0300

    fs.exists does work

--- examples/example_check_if_file_exist.py
+++ examples/example_check_if_file_exist.py
@@ -4,9 +4,7 @@ from os import environ
 with DVCFS(
     dvc_repo=f"https://{environ['GIT_TOKEN']}@github.com/covid-genomics/data-artifacts.git"
 ) as fs:
-    # for path in ["gisaid//2020-10-10.meta.csv", "gisaid\\2020-10-10.meta.csv"]:
-    #     print(fs.exists(path=path))
-    # for path in fs.walk.files():
-    #     # Print all paths in repo
-    #     print(path)
-    print(fs.exists("ada.txt"))
+    for path in ["cirk//BVic HIs_SH2021.xlsx"]:
+         print(fs.exists(path=path))
+
+        
